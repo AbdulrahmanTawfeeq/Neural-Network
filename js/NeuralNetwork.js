ccf6556863eb14715e143da353cde900875e48a1
unknown <greenzonecoc@gmail.com>
2022-05-21 01:20:15 +0300

    finishing

--- js/NeuralNetwork.js
+++ js/NeuralNetwork.js
@@ -1,23 +1,143 @@
 class NeuralNetwork {
-  static learningRate = 0.1;
+  static learningRate = 0.5;
+  #errors; // private
   constructor(inputs, hiddenNodes, outputNodes) {
+    // how many inputs, just a number
     this.inputs = inputs;
-    this.hiddenNodes = Array(hiddenNodes);
-    this.outputNodes = Array(outputNodes);
+    this.inputLayer = [];
+    this.hiddenLayer = Array(hiddenNodes);
+    this.outputLayer = Array(outputNodes);
+    this.#errors = Array(this.outputLayer.length);
 
-    for (let i = 0; i < this.hiddenNodes.length; i++) {
-      this.hiddenNodes[i] = new Perceptron(this.inputs);
+    /**
+     * filling out the hidden layer with Perceptrons
+     * weights and bias w will be set according to no. of inputs
+     */
+    for (let i = 0; i < this.hiddenLayer.length; i++) {
+      this.hiddenLayer[i] = new Perceptron(this.inputs);
     }
 
-    for (let i = 0; i < this.outputNodes.length; i++) {
-      this.outputNodes[i] = new Perceptron(this.hiddenNodes.length);
+    /**
+     * filling out the output layer with Perceptrons
+     * weights and bias w will be set according to no. of hidden layer Perceptrons
+     */
+    for (let i = 0; i < this.outputLayer.length; i++) {
+      this.outputLayer[i] = new Perceptron(this.hiddenLayer.length);
     }
   }
 
-  feedForward() {
-    //   10.13: Neural Networks: Feedforward Algorithm Part 2 - The Nature of Code: MIN: 7
+  /**
+   * sets outputs of all Perceptrons as guesses,
+   * sets the Input Layer,
+   * returns the output of the NN (output layer)
+   */
+  predict(inputList) {
+    // to avoid affecting the passed one. return new address, to avoid refrensing the same memory address
+    this.inputLayer = Array.from(inputList);
+    // to be as input list to the output layer
+    let hiddenLayerOutputs = [];
+    /**
+     * inputs to each Perceptron in the hidden layer and calc its output
+     * the output func of Perceptron will add the bias
+     */
+    for (let i = 0; i < this.hiddenLayer.length; i++) {
+      hiddenLayerOutputs.push(this.hiddenLayer[i].output(this.inputLayer));
+    }
+
+    let outputLayerOutputs = [];
+
+    /**
+     * hiddenLayerOutputs will be as inputs to each Perceptron in the output layer and calc its output
+     * the output func of Perceptron will add the bias
+     */
+    for (let i = 0; i < this.outputLayer.length; i++) {
+      outputLayerOutputs.push(this.outputLayer[i].output(hiddenLayerOutputs));
+    }
+
+    return outputLayerOutputs;
+  }
+
+  train(inputList, targets) {
+    let outputLayerOutputs = this.predict(inputList);
+
+    // getting the hidden layer outputs by accessing each Perceptron there
+    let hiddenLayerOutputs = this.hiddenLayer.map((perceptron) => {
+      return perceptron.getOutput();
+    });
+
+    // adding the bias value as we dont have bias Perceptron.
+    hiddenLayerOutputs.push(1);
+
+    // calc the error for each Perceptron in the output layer
+    for (let i = 0; i < outputLayerOutputs.length; i++) {
+      this.#errors[i] = NeuralNetwork.error(targets[i], outputLayerOutputs[i]);
+    }
+
+    /**
+     * op: output layer perceptron
+     * opw: output layer perceptron weight
+     * hp: hidden layer perceptron
+     * hpw: hidden layer perceptron weight
+     */
+    for (let op = 0; op < this.outputLayer.length; op++) {
+      let dESouurce =
+        this.#dErrorOutput(targets[op], this.outputLayer[op].getOutput()) *
+        this.#dOutputNet(this.outputLayer[op].getOutput());
+      for (let opw = 0; opw < this.outputLayer[op].getWeights().length; opw++) {
+        dESouurce *= this.#dNetSource(opw, hiddenLayerOutputs);
+
+        let newWeight =
+          this.outputLayer[op].getWeights()[opw] -
+          NeuralNetwork.learningRate * dESouurce;
+        this.outputLayer[op].setWeight(opw, newWeight);
+      }
+    }
+
+    this.inputLayer.push(1);
+
+    for (let hp = 0; hp < this.hiddenLayer.length; hp++) {
+      let dETotlal = 0;
+      let dOutputNet = this.#dOutputNet(this.hiddenLayer[hp].getOutput());
+      for (let hpw = 0; hpw < this.hiddenLayer[hp].getWeights().length; hpw++) {
+        let dNetSource = this.#dNetSource(hpw, this.inputLayer);
+        for (let op = 0; op < this.outputLayer.length; op++) {
+          dETotlal +=
+            this.#dErrorOutput(targets[op], this.outputLayer[op].getOutput()) *
+            this.#dOutputNet(this.outputLayer[op].getOutput()) *
+            this.#dNetSource(hp, this.outputLayer[op].getPreWeights()) *
+            dOutputNet *
+            dNetSource;
+        }
+
+        let newWeight =
+          this.hiddenLayer[hp].getWeights()[hpw] -
+          NeuralNetwork.learningRate * dETotlal;
+        this.hiddenLayer[hp].setWeight(hpw, newWeight);
+      }
+    }
+  }
+
+  #dErrorOutput(target, output) {
+    return -(target - output);
+  }
+
+  #dOutputNet(output) {
+    return output * (1 - output);
+  }
+
+  #dNetSource(sourceIndex, inputs) {
+    return inputs[sourceIndex];
+  }
+
+  static error(target, output) {
+    return 0.5 * (target - output) ** 2;
+  }
+
+  getErrors() {
+    return this.#errors;
+  }
+
+  getTotalError() {
+    return this.getErrors().reduce((acc, curr) => acc + curr);
   }
 }
-
-let nn = new NeuralNetwork(2, 5, 1);
-console.log(nn);
--- js/Perceptron.js
+++ js/Perceptron.js
@@ -1,5 +1,7 @@
 class Perceptron {
   #weights = []; // private
+  #preWeights;
+  #output;
   constructor(numOfInputs, history = []) {
     // Set random weights for each input and the bias if no weights
     if (history.length > 0) {
@@ -11,5 +13,40 @@ class Perceptron {
       }
       this.#weights[this.#weights.length] = Math.random() * (1 - -1) + -1; // bias weight
     }
+    this.#preWeights = Array(this.#weights.length);
+  }
+
+  output(inputList) {
+    // to avoid affecting the passed one. return new address, to avoid refrensing the same memory address
+    let inputs = Array.from(inputList);
+    // adding the bias input
+    inputs.push(1);
+    let net = 0;
+    for (let i = 0; i < inputs.length; i++) {
+      net += inputs[i] * this.#weights[i];
+    }
+    this.#output = Neuron.sigmoid(net);
+    return this.#output;
+  }
+
+  getWeights() {
+    return this.#weights;
+  }
+
+  getPreWeights() {
+    return this.#preWeights;
+  }
+
+  setWeight(weightIndex, newWeight) {
+    this.#preWeights[weightIndex] = this.#weights[weightIndex];
+    this.#weights[weightIndex] = newWeight;
+  }
+
+  getOutput() {
+    return this.#output;
+  }
+
+  static sigmoid(number) {
+    return 1 / (1 + Math.exp(-number));
   }
 }
